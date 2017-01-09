6b0867eb6cd4e9e553eb9730a056f7aa3af7cd5e
Laura <laura.strazdina@testdevlab.com>
2017-01-09 10:53:15 +0200

    2.md

--- features/screens/screen_intro.rb
+++ features/screens/screen_intro.rb
@@ -2,10 +2,24 @@ class ScreenIntro < ScreenBase
   attr_accessor :driver
   def initialize(driver)
     @base = element(:id, 'content')
+    #Izvēlos šo elementu meklēt pēc id, jo tas ir unikāls
+    @button_intro_close = element(:id, 'close_intro')
+    #Izvēlos šo elementu meklēt pēc id, jo tas ir unikāls
+    @button_intro_footer_image = element(:id, 'intro_footer_image')
+    #Izvēlos šo elementu meklēt pēc id, jo tas ir unikāls
+    @button_intro_text_header = element(:id, 'intro_text_header')
+    #Izvēlos šo elementu meklēt pēc id, jo tas ir unikāls
+    @button_intro_text_footer = element(:id, 'intro_text_footer')
+    @image_page = element(:id, 'page_image')
     @driver = driver
   end
 
   def visible?
     @driver.find_element(@base[:type], @base[:value])
+    @driver.find_element(@button_intro_close[:type], @button_intro_close[:value])
+    @driver.find_element(@button_intro_footer_image[:type], @button_intro_footer_image[:value])
+    @driver.find_element(@button_intro_text_header[:type], @button_intro_text_header[:value])
+    @driver.find_element(@button_intro_text_footer[:type], @button_intro_text_footer[:value])
+    @driver.find_element(@image_page[:type], @image_page[:value])
   end
 end
